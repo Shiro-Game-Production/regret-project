2df1f88241c37277dd7c15adaaf2cb7c55575243
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-11-19 12:03:37 +0800

    Add event data

---
+++ Assets/Scripts/Event/EventData.cs
@@ -0,0 +1,16 @@
+﻿using Actors;
+using UnityEngine;
+
+namespace Event
+{
+    [CreateAssetMenu(fileName = "NewEventData", menuName = "Dialogue/Event Data", order = 0)]
+    public class EventData : ScriptableObject
+    {
+        public enum EventState{ NotStarted, Start, Active, Finish }
+
+        public string eventName;
+        public EventState eventState = EventState.NotStarted;
+        public int triggerLimit = 1;
+        public Actor affectedActor;
+    }
+}
