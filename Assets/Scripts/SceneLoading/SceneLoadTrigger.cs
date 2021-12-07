0067faf371d81aacbd8cd2f3dd46ab8c0e809459
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-12-08 00:16:20 +0800

    Add exit game UI, scene loader, lighting, and occlusion culling

---
+++ Assets/Scripts/SceneLoading/SceneLoadTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace SceneLoading
+{
+    public class SceneLoadTrigger : SingletonBaseClass<SceneLoadTrigger>
+    {
+        #region Don't Destroy On Load
+        
+        /// <summary>
+        /// Use only 1 Scene Load Trigger from HomeScene
+        /// </summary>
+        private void SetInstance()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        #endregion
+        
+        private const string LOADING_SCENE_NAME = "LoadingScene";
+
+        private void Awake()
+        {
+            SetInstance();
+        }
+
+        /// <summary>
+        /// Load loading scene to trigger scene loader by name
+        /// </summary>
+        /// <param name="sceneName">Scene's name to load</param>
+        public void LoadScene(string sceneName)
+        {
+            LoadingData.SceneName = sceneName;
+            SceneManager.LoadScene(LOADING_SCENE_NAME);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoading/SceneLoader.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SceneLoading
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [Header("Rotation animation")]
+        [SerializeField] private RectTransform loadingImage;
+        [Range(100, 500)]
+        [SerializeField] private float rotationSpeed = 350;
+        
+        [Header("Typing animation")]
+        [SerializeField] private Text loadingText;
+        [Range(0, 1)]
+        [SerializeField] private float typingSpeed = 0.05f;
+        private bool stopAnimating;
+        
+        private const float WAIT_SECONDS = 2.0f;
+        
+        private void Start()
+        {
+            StartCoroutine(TypingAnimation());
+            StartCoroutine(LoadSceneAsync());
+        }
+
+        private void Update()
+        {
+            loadingImage.Rotate(Vector3.back * (Time.deltaTime * rotationSpeed));
+        }
+        
+        /// <summary>
+        /// Typing animation
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator TypingAnimation()
+        {
+            while (!stopAnimating)
+            {
+                loadingText.text = "Loading";
+
+                foreach (char letter in ".....")
+                {
+                    yield return new WaitForSeconds(typingSpeed);
+                    loadingText.text += letter;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Load scene asynchronously
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator LoadSceneAsync()
+        {
+            // Wait for 3 seconds
+            yield return new WaitForSeconds(WAIT_SECONDS);
+            
+            // Load scene asynchronously
+            AsyncOperation loadingScene = SceneManager.LoadSceneAsync(LoadingData.SceneName);
+
+            stopAnimating = loadingScene.isDone;
+        }
+    }
+}
