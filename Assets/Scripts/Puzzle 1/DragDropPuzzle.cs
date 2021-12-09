c72078bc01e61a672432faf8c50d3e9d047c4823
Aurelius Dilectio Yuanata Djatmiko <aureliusdilectioyd@gmail.com>
2021-12-09 15:47:35 +0700

    Add drag & drop puzzle

---
+++ Assets/Scripts/Puzzle 1/DragDropPuzzle.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDropPuzzle : MonoBehaviour
+{
+    Vector3 offset;
+
+    public GameObject panel;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        PuzzleClear();
+    }
+
+    private void OnMouseDown() 
+    {
+        offset = transform.position - MouseWorldPosition();
+    }
+
+    private void OnMouseDrag() 
+    {
+        transform.position = MouseWorldPosition() + offset;
+    }
+
+    Vector3 MouseWorldPosition()
+    {
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPos);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "Trashbin")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void PuzzleClear()
+    {
+        if(GameObject.FindGameObjectsWithTag("Trash").Length == 1)
+        {
+            Debug.Log("Puzzle Cleared");
+            panel.gameObject.SetActive(true);
+            //kode ke dialog di sini
+        }
+    }
+
+}
