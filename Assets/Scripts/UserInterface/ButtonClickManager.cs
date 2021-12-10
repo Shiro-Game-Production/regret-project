f3793735501ff3cae3e511757b765eff2b85ce78
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-12-10 22:52:10 +0800

    Remove pause and resume method

--- Assets/Scripts/UserInterface/ButtonClickManager.cs
+++ Assets/Scripts/UserInterface/ButtonClickManager.cs
@@ -6,28 +6,6 @@ namespace UserInterface
 {
     public class ButtonClickManager : MonoBehaviour
     {
-        [SerializeField] private CanvasGroup pauseCanvasGroup;
-        
-        /// <summary>
-        /// Pause or resume game
-        /// </summary>
-        /// <param name="pauseGame">True to pause, False to resume</param>
-        public void PauseGame(bool pauseGame)
-        {
-            if(pauseGame)
-            {
-                StartCoroutine(FadingEffect.FadeIn(pauseCanvasGroup, 
-                    afterEffect: () => Time.timeScale = 0f)
-                );
-            }
-            else
-            {
-                StartCoroutine(FadingEffect.FadeOut(pauseCanvasGroup, 
-                    beforeEffect: () => Time.timeScale = 1f)
-                );
-            }
-        }
-        
         public void ShowPrompts(CanvasGroup canvasGroup)
         {
             StartCoroutine(FadingEffect.FadeIn(canvasGroup));
