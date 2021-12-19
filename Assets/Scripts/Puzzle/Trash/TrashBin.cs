666109cf01ad99d45fac5bf7d090555e1dc09ab6
alfianAH <alfian.hamdani04@gmail.com>
2021-12-19 15:58:18 +0800

    Add finish condition in trash puzzle

--- Assets/Scripts/Puzzle/Trash/TrashBin.cs
+++ Assets/Scripts/Puzzle/Trash/TrashBin.cs
@@ -6,11 +6,30 @@ namespace Puzzle.Trash{
     public class TrashBin : MonoBehaviour {
         [SerializeField] private List<Trash> trashList;
 
-        private void OnTriggerEnter2D(Collider2D other) {
-            if(other.GetComponent<Trash>()){
-                Debug.Log("Trash");
-                other.gameObject.SetActive(false);
+        private Dictionary<Trash, bool> trashDict;
+
+        private void Awake() {
+            trashDict = new Dictionary<Trash, bool>();
+
+            foreach(Trash trash in trashList){
+                trashDict.Add(trash, false);
             }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            Trash trash = other.GetComponent<Trash>();
+            if(!trash) return;
+
+            // Set trash's condition
+            trashDict[trash] = true;
+            other.gameObject.SetActive(false);
+
+            // Check finish condition
+            foreach(bool value in trashDict.Values){
+                if(!value) return;
+            }
+
+            Debug.Log("Puzzle done");
         }
     }
 }
