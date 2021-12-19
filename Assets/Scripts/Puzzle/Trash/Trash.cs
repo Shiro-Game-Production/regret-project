7502ab1f970f4b9881718970718b0305f5e1ea0a
alfianAH <alfian.hamdani04@gmail.com>
2021-12-19 15:25:50 +0800

    Make sure trash is in parent bounds

--- Assets/Scripts/Puzzle/Trash/Trash.cs
+++ Assets/Scripts/Puzzle/Trash/Trash.cs
@@ -6,16 +6,17 @@ namespace Puzzle.Trash {
         IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Canvas canvas;
+        [SerializeField] private RectTransform parentRectTransform;
 
-        private Camera mainCamera;
+        private Bounds parentBounds;
         private RectTransform rectTransform;
 
         private void Awake() {
-            mainCamera = Camera.main;
             rectTransform = GetComponent<RectTransform>();
-            Debug.Log($"Scale factor: {canvas.scaleFactor}");
-            Debug.Log($"Screen width: {Screen.width}");
-            Debug.Log($"Screen height: {Screen.height}");
+
+            parentBounds = new Bounds(
+                parentRectTransform.anchoredPosition,
+                parentRectTransform.rect.size);
         }
 
         public void OnBeginDrag(PointerEventData eventData)
@@ -26,11 +27,46 @@ namespace Puzzle.Trash {
         public void OnDrag(PointerEventData eventData)
         {
             rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            rectTransform.anchoredPosition = CheckPosition(rectTransform.anchoredPosition);
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             Debug.Log("End drag");
+        }
+        
+        /// <summary>
+        /// Make sure trash's position is in parentBounds
+        /// </summary>
+        /// <param name="position">Trash's anchored position</param>
+        /// <returns>Trash's anchored position</returns>
+        private Vector3 CheckPosition(Vector3 position){
+            float xAxis = position.x;
+            float yAxis = position.y;
+
+            // Left side (min)
+            if(xAxis < 0){
+                if(xAxis < parentBounds.min.x){
+                    position.x = parentBounds.min.x;
+                }
+            } else if(xAxis > 0) { // Right side (max)
+                if(xAxis > parentBounds.max.x){
+                    position.x = parentBounds.max.x;
+                }
+            }
+
+            // Lower side (min)
+            if(yAxis < 0){
+                if(yAxis < parentBounds.min.y){
+                    position.y = parentBounds.min.y;
+                }
+            } else if(yAxis > 0) { // Upper side (max)
+                if(yAxis > parentBounds.max.y){
+                    position.y = parentBounds.max.y;
+                }
+            }
+
+            return position;
         }
     }
 }
