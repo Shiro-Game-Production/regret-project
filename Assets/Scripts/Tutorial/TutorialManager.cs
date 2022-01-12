3af88e7411e711c07971299003135098fea5fbf5
alfianAH <alfian.hamdani04@gmail.com>
2022-01-12 17:58:30 +0800

    Move home screen sprites and optimize tutorial manager

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Player;
 using Dialogue;
@@ -8,45 +6,27 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject popUps;
 
-    private bool hasShown = false;
-
     //Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Invoke(nameof(StartTutorial), 0.04f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartTutorial()
     {
-        Invoke("StartTutorial", 0.04f);
-        
-        //StopTutorial();
-    }
-
-    void StartTutorial()
-    {
-        if(!DialogueManager.Instance.DialogueIsPlaying && !PlayerMovement.Instance.Movement.IsWalking && !hasShown)
+        if(!DialogueManager.Instance.DialogueIsPlaying && 
+            !PlayerMovement.Instance.Movement.IsWalking)
         {
             
             popUps.SetActive(true);
-            hasShown = true;
             Debug.Log("Tutorial Nyala");
         }
 
-        else if(DialogueManager.Instance.DialogueIsPlaying || PlayerMovement.Instance.Movement.IsWalking)
+        else if(DialogueManager.Instance.DialogueIsPlaying || 
+            PlayerMovement.Instance.Movement.IsWalking)
         {
             popUps.SetActive(false);
             Debug.Log("Tutorial Mati");
         }
     }
-
-    // void StopTutorial()
-    // {
-    //     if(DialogueManager.Instance.DialogueIsPlaying || PlayerMovement.Instance.IsWalking)
-    //     {
-    //         popUps.SetActive(false);
-    //         Debug.Log("Tutorial Mati");
-    //     }
-    // }
 }
